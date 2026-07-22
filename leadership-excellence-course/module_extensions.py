6a7206fae01d643ca279e420021643b19248a553
somasundaramntlifecoach-ux <somasundaramnt.lifecoach@gmail.com>
2026-07-22 06:52:41 +0530

    Delete leadership-excellence-course directory

--- leadership-excellence-course/module_extensions.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from module_extensions_part1 import EXTENSIONS as _P1
-from module_extensions_part2 import EXTENSIONS_PART2 as _P2
-
-EXTENSIONS = {}
-EXTENSIONS.update(_P1)
-EXTENSIONS.update(_P2)
